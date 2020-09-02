6961e947023c4faf8d79c62cf4b092dac1058972
ishimot8001 <ishimoto8001@gmail.com>
2020-09-02 16:20:27 +0900

    SyntaxErrorを手に入れた！　完全理解！　エラーメッセージは音読しよう！

--- sample01.py
+++ sample01.py
@@ -4,3 +4,9 @@ print('Tom')
 
 # コメント：半角の # を書くと、　それ以降は無視されます
 # メモを残す(勉強用)
+
+# わざとエラーを起こそう！最後のカッコが抜けている！！
+# SyntaxError: unexpected EOF while parsing
+# SyntaxError: Syntaxは文法や構文って意味⇒構文エラー
+#print('hello'
+
