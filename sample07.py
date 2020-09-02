ac433c58e023885d3743f9cf56c6880131365819
ishimot8001 <ishimoto8001@gmail.com>
2020-09-02 17:55:23 +0900

    if-else if-elif-else

---
+++ sample07.py
@@ -0,0 +1,20 @@
+# 条件分岐その2: 「もし○○なら、☆☆、そうでなければ△△」(if-else)
+# score = 80
+#
+# if score >= 70:
+#     print('Great!!')
+# else:
+#     print('So so')
+
+# 条件分岐その2: 「もし○○なら、☆☆、
+#                そうでなくて□□なら××
+#                どれでもなければyy」(if-elif-else)
+
+score = 40
+
+if score >= 70:
+    print('Great!!')
+elif score >= 40:
+    print('So so')
+else:
+    print('Bad...')
