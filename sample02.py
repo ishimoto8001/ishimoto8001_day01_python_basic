3925f07226629726ac11cc5e1c128e09b4fabdf9
ishimot8001 <ishimoto8001@gmail.com>
2020-09-02 16:53:00 +0900

    NameErrorおぼえた！https://docs.python.org/ja/3/library/exceptions.html

--- sample02.py
+++ sample02.py
@@ -8,8 +8,18 @@ print(a + b)
 # 意味のある名付けの例
 unit_price = 300    # 単価
 quantity = 5        # 量
+
 total_price = unit_price * quantity
 print(total_price)  # 1500
+
+# わざとErrorのコーナー
+# NameError: name 'age' is not defined
+# 未定義の変数 age を使っている場合
+# print(age)
+
+
+
+
 
 
 
