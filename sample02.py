705af4c1656e671472d8e5fbc226d59b0660068b
ishimot8001 <ishimoto8001@gmail.com>
2020-09-02 16:46:18 +0900

    データに名前をつけられるのだから、よりよい名前にしたいよね！

--- sample02.py
+++ sample02.py
@@ -4,3 +4,12 @@
 a = 3  # 変数a に 3 を代入する: 3というデータに対して「a」という名前をつけた
 b = 4
 print(a + b)
+
+# 意味のある名付けの例
+unit_price = 300    # 単価
+quantity = 5        # 量
+total_price = unit_price * quantity
+print(total_price)  # 1500
+
+
+
