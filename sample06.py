7aff81bbb2e6b0a9ed1638e33e81da48ec4af333
ishimot8001 <ishimoto8001@gmail.com>
2020-09-02 17:48:49 +0900

    コロン・インデント忘れがち！

---
+++ sample06.py
@@ -0,0 +1,15 @@
+# # if関連のエラーをわざと起こすコーナー！
+# # 「:」(コロン)を忘れてしまう(文法ミス！)
+# # SyntaxError: invalid syntax
+#
+# total_price = 777
+#
+# if total_price == 777
+#     print('大当たり！！！')
+
+# # その2: インデント(字下げ)できていない！
+# # IndentationError: expected an indented block
+# total_price = 777
+#
+# if total_price == 777:
+# print('大当たり！！！')
